933562c6858521c642b0a8a08dd0c6cc22780219
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-07-14 15:32:44 -0400

    Update maleditor.py

--- mal-factory/maleditor.py
+++ mal-factory/maleditor.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python
+
 import os
 import sys
 
-
 def options():
-    print("\tOptions: ")
-
-    print("\t\t1) Save file")
-    print("\t\t2) Undo last line")
-    print("\t\t3) Open from txt file")
-
-    print("\n\t\t99) Exit MalEditor without saving")
-
-
-def clear():
-    for i in range(3):
-        os.system("clear")
-
+    print("  [-] Options: \n")
+    print("  [s] Save file \t [u] Undo last line")
+    print("  [o] Open from txt file \t [e] Exit MalEditor")
 
 def print_file(file: list):
     for element in file:
         print(element)
 
-    def main():
-        print("[+] Starting Mal-editor ")
-        options()
-        whole_file = []
-        try:
-            while True:
-                command = input("MalEditor >")
-                if command == "99":
-                    sys.exit()
-                elif command == "2":
-                    whole_file.pop()
-                    print_file(whole_file)
-
-
-                else:
-                    clear()
-                    whole_file.append(command)
-                    print_file(whole_file)
+def main():
+    print("[+] Starting Mal-editor ")
+    options()
+    whole_file = []
+    try:
+        while True:
+           command = input("MalEditor >")
+           if command == "99" or :
+                sys.exit()
+           elif command == "2":
+                whole_file.pop()
+                print_file(whole_file)
+           else:
+                clear()
+                whole_file.append(command)
+                print_file(whole_file)
         except KeyboardInterrupt:
             sys.exit()
+
+def startup():
+    core.clear()
+    main()
