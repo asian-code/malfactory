fd4bb709b6cacec6a4ce236e8119eca20e528464
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-14 20:54:38 -0400

    Update malfactory.py

--- mal-factory/malfactory.py
+++ mal-factory/malfactory.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 import main
-printf '\e[8;50;100t' # makes terminal size bigger to see YOUR GIANT ASS LOGOS, your welcome you cunt XD
+# resize -s 35 100
+
+printf '\e[8;35;100t' # makes terminal size bigger to see YOUR GIANT ASS LOGOS, your welcome you cunt XD
 main.startup()
