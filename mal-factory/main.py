9225449b3c5c793e47d96a4ca7407888e631eefc
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-18 17:38:54 -0400

    Update main.py

--- mal-factory/main.py
+++ mal-factory/main.py
@@ -15,7 +15,8 @@ rr = core.rr
 bold = core.bold
 ul = core.ul
 line = purple + bold + "--------------------------------------------------------------------------------------" + rr
-tool_version=0.1
+tool_version = 0.1
+
 
 def clear():
     for i in range(4):
@@ -23,14 +24,14 @@ def clear():
 
 
 def options():
-    
-    print(" " + purple + bold + "Tool Version: " + rr + str(tool_version) +"\n") 
+    print(" " + purple + bold + "Tool Version: " + rr + str(tool_version) + "\n")
     print(" " + ul + "Please Select From The Menu" + rr + "\n")
     print(rr + "   [" + purple + "1" + rr + "]\t Malware Terminal IDE")
     print("   [" + purple + "2" + rr + "]\t Basic Malware Templates")
     print("   [" + purple + "3" + rr + "]\t Send Malware With Email w/ Gmail")
     print("   [" + purple + "r" + rr + "]\t Reloads The Screen")
-    print("   [" + purple + "u" + rr + "]\t Updates The Tool")
+
+    print("   [" + red + "u" + rr + "]\t Uninstall Malfactory")
     print("\n   [" + purple + "99" + rr + "]\t Exit \n")
 
 
@@ -52,13 +53,14 @@ def main():
     except KeyboardInterrupt:
         core.quit()
     except Exception:
-        print(rr + "\n[" + red + "+" + rr + "] Error: something went wrong \n")# Need general error message since this try is the whole program instead of a specific block of code
+        print(
+            rr + "\n[" + red + "+" + rr + "] Error: something went wrong \n")  # Need general error message since this try is the whole program instead of a specific block of code
         raise
 
 
 def startup():
     os.system("resize -s 40 86")
-    clear()
+    # clear()
     print(line)
     core.randomlogo()
     print(line + "\n")
