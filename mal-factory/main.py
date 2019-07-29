4264706ccef4bf9816d1128942f1073d5342d185
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-29 12:59:49 -0400

    Update main.py

--- mal-factory/main.py
+++ mal-factory/main.py
@@ -5,6 +5,7 @@ import maleditor
 import gmailemail
 import os
 import sys
+import platform
 
 pink = core.pink
 green = core.lgreen
@@ -18,20 +19,26 @@ bold = core.bold
 ul = core.ul
 line = purple + bold + "--------------------------------------------------------------------------------------" + rr
 toolVersion = 0  # get tool version from update.py not in main.py
+operating_system = platform.system()
 
 
 def get_install_location():
     # allows for python to find installation folder
     loc = ""
     try:
-        file = open(os.path.join(sys.path[0], "location.txt"), "r")
+        if operating_system == "Linux":
+            file = open(os.path.join(sys.path[0], "location.txt"), "r")
+        else:
+            file = open("location.txt")
         loc = file.read()
         file.close()
-        locationfound = (rr + "[+] Sucessfully found location.txt!" + rr)
+        # locationfound = (rr + "[+] Sucessfully found location.txt!" + rr)
     except FileNotFoundError:
-        locationfound = (red + "[!] Error, location.txt is not located" + rr)
-    print(core.lpurple + "[+] Checking for updates... \t\t | \t" + locationfound)
+        print(red + "[!] Cannot find location.txt" + rr)
+        # locationfound = (red + "[!] Error, location.txt is not located" + rr)
+        # print(core.lpurple + "[+] Checking for updates... \t\t | \t" + locationfound)
     return loc
+
 
 def clear():
     for i in range(4):
@@ -60,7 +67,6 @@ def helpMenu():
     print("Help menu")
     print("[1] MalEditor -is used for making malware using \"Malscript\", you can write and save your scripts here")
     print("[2] Reloads The Screen -is used for refreshing the screen")
-# etc , add more stuff here
 
 
 def main():  # takes in user input and check for commands
@@ -80,7 +86,8 @@ def main():  # takes in user input and check for commands
             elif command == "help":
                 helpMenu()
             elif command == "z":
-                print("\n" + rr + "Please submit any bugs/issues/glitches at this link:\n" + cyan + ul + bold + "https://github.com/asian-code/malfactory/issues" + rr + "\n")
+                print(
+                    "\n" + rr + "Please submit any bugs/issues/glitches at this link:\n" + cyan + ul + bold + "https://github.com/asian-code/malfactory/issues" + rr + "\n")
             elif command == "u":
                 dontgo = input(red + bold + "[!] Are you sure you want to UNINSTALL Malfactory? (y/n):" + rr)
                 dontgo = dontgo.lower()
@@ -88,20 +95,23 @@ def main():  # takes in user input and check for commands
                     loc = get_install_location()
                     uninstall_loc = loc + "/uninstaller"
                     print(blue + "[+] Checking for uninstaller in " + rr + uninstall_loc)
-                    
-                    sys.path.append(loc)
+
+                    sys.path.append(uninstall_loc)
                     try:
                         import uninstaller
+                        print("We are sad to see you go, if there are issues please report them to the github page")
+                        sys.exit()
                     except ImportError:
                         print(red + "[!] Error trying to uninstall" + rr)
-            
+
             else:
                 print(rr + red + "\nSorry, " + command + " is not a command.\n")
 
     except KeyboardInterrupt:
         core.quit()
     except Exception:
-        print(rr + "\n[" + red + "+" + rr + "] Error: something went wrong \n")  # Need general error message since this try is the whole program instead of a specific block of code
+        print(
+            rr + "\n[" + red + "+" + rr + "] Error: something went wrong \n")  # Need general error message since this try is the whole program instead of a specific block of code
         raise
 
 
@@ -112,10 +122,10 @@ def startup():  # display logo and options
     core.randomlogo()
     print(line + "\n")
     core.textlogo()
-    #print(green + "[+] Checking for updates... " + green + "\t | \t" + locationfound(torfanswer))
-    
+    # print(green + "[+] Checking for updates... " + green + "\t | \t" + locationfound(torfanswer))
+
     # testing code{
-    
+
     loc = get_install_location()
     # if loc != "":
     updater_loc = loc + "/update"
@@ -125,11 +135,14 @@ def startup():  # display logo and options
         update.start_update()  # not force the update unless really outdated
         global toolVersion
         toolVersion = update.tool_version
-    
+
     except ImportError:
         toolVersion = red + "[!] Error trying to update." + rr
 
-    print(core.lpurple + "[+] Checking for updater in " + rr + updater_loc + core.lpurple + "\t | \t" + rr + toolVersion)
+    print(
+        core.lpurple + "[+] Checking for updater in " + rr + updater_loc + core.lpurple + "\t | \t" + rr + str(
+            toolVersion))
+
 
 # } testing code
 
