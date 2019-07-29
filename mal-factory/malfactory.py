66e0e4f3efaed8a3a53093a422e469aae812218c
asian-code <52612730+asian-code@users.noreply.github.com>
2019-07-29 15:02:35 -0400

    Update malfactory.py

--- mal-factory/malfactory.py
+++ mal-factory/malfactory.py
@@ -2,6 +2,23 @@
 
 import main
 import core
+# test
+import sys
+import os
 
 core.laughingskull()
+print("Testing code for mac{")
+print("Method 1 test")
+try:
+    test1 = open("location.txt", "r")
+    test1.close()
+except:
+    print("Error with Method 1")
+print("Method 2 test")
+try:
+    test1 = open(os.path.join(os.path[0], "location.txt"), "r")
+    test1.close()
+except:
+    print("Error with Method 2")
+    print("Testing code for mac END")
 main.startup()
